fe30d7470766e3aed07485c22ae9bd834c5116b6
Allan Leherpeux <allan.leherpeuxprv@gmail.com>
2023-02-06 12:48:38 +0100

    [ADD] player

---
+++ index.js
@@ -0,0 +1,32 @@
+const canvas = document.querySelector('canvas');
+
+const c = canvas.getContext('2d');
+
+canvas.width = 1024;
+canvas.height = 576;
+
+c.fillStyle = "white";
+c.fillRect(0, 0, canvas.width, canvas.height);
+
+const image = new Image();
+image.src = "./Images/Pokeflop_Map.png";
+const playerImage = new Image();
+playerImage.src = "./Images/playerDown.png";
+
+image.onload = () => {
+    c.drawImage(image, -400, -350);
+    c.drawImage(
+        playerImage,
+        0,
+        0,
+        playerImage.width / 4,
+        playerImage.height,
+        canvas.width / 2 - (playerImage.width / 4) / 2,
+        canvas.height / 2 - playerImage.height / 2,
+        playerImage.width / 4,
+        playerImage.height
+
+    );
+}
+
+
